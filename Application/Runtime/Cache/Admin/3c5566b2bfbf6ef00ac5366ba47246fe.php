a0b7b9ba14c1b691cf9b9c2ce556ff622876f6f5
bnqboy <Haiwen.Ji@b-and-qchina.com>
2016-09-13 18:47:41 +0800

    login background

--- Application/Runtime/Cache/Admin/3c5566b2bfbf6ef00ac5366ba47246fe.php
+++ Application/Runtime/Cache/Admin/3c5566b2bfbf6ef00ac5366ba47246fe.php
@@ -1,14 +1,14 @@
-<html lang="cn">
+<?php if (!defined('THINK_PATH')) exit();?><html lang="cn">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>后台管理系统</title>
-    <link href="__PUBLIC__/Framework/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="__CSS__/login.css" rel="stylesheet">
-    <script src="__PUBLIC__/Javascript/Public/jquery-1.10.2.min.js"></script>
-    <script src="__PUBLIC__/Framework/Bootstrap/js/bootstrap.min.js"></script>
+    <link href="/Public/Framework/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/Public/Stylesheet/Admin/login.css" rel="stylesheet">
+    <script src="/Public/Javascript/Public/jquery-1.10.2.min.js"></script>
+    <script src="/Public/Framework/Bootstrap/js/bootstrap.min.js"></script>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
